62f6cb36381934abc1392a91372d660fd38f1050
User <gicagarcia58@gmail.com>
2024-04-03 19:21:22 -0300

    add ContactActivity

--- app/src/main/java/com/example/contactspdm/ui/ContactActivity.kt
+++ app/src/main/java/com/example/contactspdm/ui/ContactActivity.kt
@@ -1,4 +1,31 @@
 package com.example.contactspdm.ui
 
-class ContactActivity {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.contactspdm.databinding.ActivityContactBinding
+import com.example.contactspdm.model.Contact
+
+class ContactActivity: AppCompatActivity() {
+    private val acb: ActivityContactBinding by lazy{
+        ActivityContactBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(acb.root)
+
+        acb.saveBt.setOnClickListener{
+            val infos = Bundle()
+            val resultadoIntent = Intent()
+            infos.putString("name", acb.nameEt.text.toString())
+            infos.putString("address", acb.addressEt.text.toString())
+            infos.putString("phone", acb.phoneEt.text.toString())
+            infos.putString("email", acb.emailEt.text.toString())
+            resultadoIntent.putExtra("bundle", infos).also {
+                setResult(RESULT_OK, it)
+                finish()
+            }
+        }
+    }
 }
